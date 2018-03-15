1448d4be3087806d34211f5dd59959eb33095701
ManetsMaria <l.i.n.k.i.n.m@mail.ru>
2018-03-15 22:03:42 +0300

    make develop conflict

--- app/src/main/java/com/example/manet/task3_serverremote/Git.java
+++ app/src/main/java/com/example/manet/task3_serverremote/Git.java
@@ -3,8 +3,11 @@ package com.example.manet.task3_serverremote;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Git extends AppCompatActivity {
+
+    private static final int ONE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,6 @@ public class Git extends AppCompatActivity {
     }
 
     public void showMessage(View view){
-
+        Toast.makeText(this, String.valueOf(ONE), Toast.LENGTH_SHORT).show();
     }
 }
