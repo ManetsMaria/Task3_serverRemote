6fc7ee1c97854386037b66df2618414a9bf10135
ManetsMaria <l.i.n.k.i.n.m@mail.ru>
2018-03-15 21:48:25 +0300

    init commit

---
+++ app/src/main/java/com/example/manet/task3_serverremote/Git.java
@@ -0,0 +1,18 @@
+package com.example.manet.task3_serverremote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Git extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git);
+    }
+
+    public void showMessage(View view){
+
+    }
+}
